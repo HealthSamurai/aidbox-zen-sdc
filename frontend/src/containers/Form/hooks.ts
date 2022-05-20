c97874562c0cfe8e7cc8331e72cdb499bdc655df
Ilya Beda <ir4y.ix@gmail.com>
2022-05-20 15:51:18 +0200

    WIP load from metadata from zen

---
+++ frontend/src/containers/Form/hooks.ts
@@ -0,0 +1,9 @@
+import { useService } from "aidbox-react/lib/hooks/service";
+import { service } from "aidbox-react/lib/services/service";
+import { useParams } from "react-router";
+
+export function useForm(){
+    const {nameSpace,name} = useParams()
+    const form = useService(() => service({url: '$zen-get2', params: {symbol: `${nameSpace}/${name}`}}));
+    return form;
+}
---
+++ frontend/src/containers/Form/index.tsx
@@ -0,0 +1,10 @@
+import { RenderRemoteData } from "aidbox-react/lib/components/RenderRemoteData";
+import { Link } from "react-router-dom";
+import { useForm } from "./hooks";
+
+export function Form(){
+    const [formRD] = useForm();
+    return <div>
+        <Link to="/">{`<- Back`}</Link>
+        <RenderRemoteData remoteData={formRD}>{(form) => <pre>{JSON.stringify(form.model.layout, undefined, 4)}</pre>}</RenderRemoteData></div>
+}
